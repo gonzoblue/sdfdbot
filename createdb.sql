c67544b4f30337ae3ee0289674d5b4f9c3b7ba96
gonzoblue <nick0909@gmail.com>
2017-08-25 02:05:34 +0000

    v10

--- createdb.sql
+++ createdb.sql
@@ -1,5 +1,5 @@
 create database sdfd;
 use sdfd;
-create table calls (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, date CHAR(25), calltype VARCHAR(50), street VARCHAR(50), crossstreet VARCHAR(100), unitids VARCHAR(500), numunits INT(3));
-insert into calls (id, date, calltype, street, crossstreet, unitids, numunits) values (NULL, '11/11/2001 11:11:11 PM', 'Medical', 'TEST ST', 'CROSS DR', 'E00, M00', '2');
+create table calls (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, calldate CHAR(25), calltype VARCHAR(50), street VARCHAR(50), crossstreet VARCHAR(100), unitids VARCHAR(500), numunits TINYINT(3), processtime TIMESTAMP, alertsent TINYINT(1);
+insert into calls (id, calldate, calltype, street, crossstreet, unitids, numunits, processdate, alertsent) values (NULL, '11/11/2001 11:11:11 PM', 'Medical', 'TEST ST', 'CROSS DR', 'E00, M00', '2', '2001-11-11 01:01:01', '0');
 exit;
--- sdfd.py
+++ sdfd.py
@@ -16,6 +16,8 @@ def inciAlert( fullCall ):
     print fullCall
   elif numUnits > 9:
     print fullCall
+  else:
+    print  "--" + fullCall
   return
 
 # Dump the HTML, turn in to BeautifulSoup object, find and save the table
@@ -34,19 +36,19 @@ for row in table.findAll("tr"):
   call = row.findAll("td")
   if len(call) == 5: 
     date = str(call[0].get_text().strip())
-    type = str(call[1].get_text().strip())
+    calltype = str(call[1].get_text().strip())
     street = str(call[2].get_text().strip())
     cross = str(call[3].get_text().strip())
-    unit = str(call[4].get_text().strip())
+    unitid = str(call[4].get_text().strip())
 
 # Build the incident string, adding units each time. If a new incident is found, alert/print the call.
     if date != prevDate:
       if prevDate != "none":
         inciAlert(callDesc)
-      callDesc = "[" + type + "] " + street + " - " + unit
+      callDesc = "[" + calltype + "] " + street + " - " + unitid
       numUnits = 1
     else:
-      callDesc = callDesc + ", " + unit
+      callDesc = callDesc + ", " + unitid
       numUnits = numUnits + 1
     prevDate = date
 # Alert/print the last call built
