8f86d881b8b40350a4d882ecb90e0ed3a7932d3d
Alex <gonzoblue@users.noreply.github.com>
2017-09-22 11:33:27 -0700

    v14
    Changed to use pulsepoint data.
    Major rewrite of call saving and alerting functions.

--- sdfd.py
+++ sdfd.py
@@ -1,148 +1,140 @@
 #https://github.com/gonzoblue/sdfdbot
-#Automated SDFD incident page scraping and alerting
+#Automated SDFD pulsepoint scraping and alerting
 #Not official or affiliated with SDFD in any way
 
-#Set up the database
-import sqlite3
-callsdb_file = '/home/pi/sdfdbot/stable/callsdb.sqlite'
-db = sqlite3.connect(callsdb_file, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-c = db.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS calls (dbid INTEGER PRIMARY KEY, dbcalldate TEXT, dbcalltype TEXT, dbstreet TEXT, dbcross TEXT, dbunitids TEXT, dbnumunits INTEGER, dbupdatetime timestamp, dbalertsent INTEGER DEFAULT 0)')
-db.commit()
-
-#Function to alert on a fully built interesting call
-def storeCall(fullCall):
-#  coolCall = ['Fire', 'CPTR', 'Sdge']
-#  if any(word in fullCall for word in coolCall):
-#    print fullCall
-#  elif numUnits > 9:
-#    print fullCall
-#  else:
-#    print  "--" + fullCall
-  fullCall = fullCall + " (" + str(numUnits) + ") @ " + savedCallDate
-  c.execute('SELECT dbid FROM calls WHERE dbcalldate=?', (savedCallDate,))
-  callid = c.fetchone()
-  now = datetime.now()
-  if callid > 0:
-    callChanged = 0
-    c.execute('SELECT * FROM calls WHERE dbid=?', (callid))
-    oldEntry = c.fetchone()
-    print "=" + str(oldEntry)
-    oldCallType = str(oldEntry[2])
-    oldStreet = str(oldEntry[3])
-    oldCross = str(oldEntry[4])
-    oldNumUnits = oldEntry[6]
-# Check for updates to old calls
-    if oldCallType != savedCallType:
-      c.execute('UPDATE calls SET dbcalltype=? where dbid=?', (savedCallType, callid[0]))
-      db.commit()
-      callChanged=1
-    elif oldStreet != savedCallStreet:
-      c.execute('UPDATE calls SET dbstreet=? where dbid=?', (savedCallStreet, callid[0]))
-      db.commit()
-      callChanged=1
-    elif oldCross != savedCallCross:
-      c.execute('UPDATE calls SET dbcross=? where dbid=?', (savedCallCross, callid[0]))
-      db.commit()
-      callChanged=1
-    elif oldNumUnits != numUnits:
-      c.execute('UPDATE calls SET dbunitids=? where dbid=?', (units, callid[0]))
-      c.execute('UPDATE calls SET dbnumunits=? where dbid=?', (numUnits, callid[0]))
-      callChanged=1
-    if callChanged==1:
-      c.execute('UPDATE calls SET dbupdatetime=? where dbid=?', (now, callid[0]))
-      db.commit()
-      c.execute('SELECT * FROM calls WHERE dbcalldate=?', (savedCallDate,))
-      print "$" + str(c.fetchone())
-  else:
-#    print "Saving" + savedCallType + " " + savedCallStreet + " as " + savedCallDate + " with timestamp " + now.strftime("%Y-%m-%d %H:%M:%S")
-    c.execute('INSERT INTO calls(dbcalldate, dbcalltype, dbstreet, dbcross, dbunitids, dbnumunits, dbupdatetime) VALUES(?,?,?,?,?,?,?)', (savedCallDate, savedCallType, savedCallStreet, savedCallCross, units, numUnits, now))
-    db.commit()
-    c.execute('SELECT * FROM calls WHERE dbcalldate=?', (savedCallDate,))
-    print "+" + str(c.fetchone())
-    sendAlert(fullCall)
-
-def sendAlert(fullCall):
-  tweet = twitter.Api(consumer_key='1',consumer_secret='2',access_token_key='3',access_token_secret='4')
-  status = tweet.PostUpdate(fullCall)
-  print "Tweet sent: " + status.text
-  coolCall = ['CPTR', 'CRATER', 'Fire', 'Residential', 'Structure Fire', 'Sdge', 'PENTICTON', 'CAPRICORN']
-  if any(word in fullCall for word in coolCall):
-    sendEmail()
-  elif numUnits > 10:
-    sendEmail()
-  return
-
-def sendEmail():
-  print "Emailing Call Type: [" + savedCallType + "]"
-  fromaddr = "me@mail.com"
-  toaddr = "you@mail.com"
-  msg = MIMEMultipart()
-  msg['From'] = fromaddr
-  msg['To'] = toaddr
-  msg['Subject']= savedCallType + " | " + savedCallStreet + " | " + str(numUnits)
-  body = savedCallType + " | " + units
-  msg.attach(MIMEText(body, 'plain'))
-  email = smtplib.SMTP('mail.com', 587)
-  email.ehlo()
-  email.starttls()
-  email.ehlo()
-  email.login("me@mail.com","mypass")
-  text = msg.as_string()
-  email.sendmail("me@mail.com", "you@mail.com", text)
-  email.quit()
-  return
-
-
-###Main:
+#Imports
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from secrets import EMAIL_FROM, EMAIL_TO, EMAIL_PASS, EMAIL_SERVER, TWITTER_CONSUMERKEY, TWITTER_CONSUMERSECRET, TWITTER_ACCESSKEY, TWITTER_ACCESSSECRET, COOLCALL, DBPATH
 import requests
 import twitter
 from datetime import datetime
 from bs4 import BeautifulSoup
-# Dump the HTML, turn in to BeautifulSoup object, find and save the table
-r = requests.get('http://apps.sandiego.gov/sdfiredispatch/')
-#r = requests.get('http://localhost/sdfd.html')
+import sqlite3
+
+#Set up the database
+callsdb_file = DBPATH + "/callsdb.sqlite"
+db = sqlite3.connect(callsdb_file, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+c = db.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS calls (dbid INTEGER PRIMARY KEY, dbcalldate TEXT, dbcalltype TEXT, dbstreet TEXT, dblat REAL, dblong REAL, dbunitids TEXT, dbnumunits INTEGER, dbupdatetime timestamp)')
+db.commit()
+
+def storeCall():
+	c.execute('SELECT dbid FROM calls WHERE dbcalldate=?', (calldate,)) #search for the call in the DB
+	thisCall = c.fetchone()
+	now = datetime.now()
+	if thisCall > 0:  #if call exists in DB
+		callChanged = 0
+		whatChanged = " "
+		c.execute('SELECT * FROM calls WHERE dbid=?', (thisCall))
+		oldEntry = c.fetchone() # save the old DB entry for comparison
+		print "=" + str(oldEntry)
+		oldCallType = str(oldEntry[2])
+		oldAddress = str(oldEntry[3])
+		oldLat = float(oldEntry[4])
+		oldLong = float(oldEntry[5])
+		oldNumUnits = oldEntry[7]
+		if oldCallType != calltype: #call exists, check for updated call type
+			c.execute('UPDATE calls SET dbcalltype=? where dbid=?', (calltype, thisCall[0]))
+			db.commit()
+			callChanged=1
+			whatChanged = whatChanged + "Type "
+		if oldAddress != address:
+			c.execute('UPDATE calls SET dbstreet=? where dbid=?', (address, thisCall[0]))
+			db.commit()
+			callChanged=1
+			whatChanged = whatChanged + "Street "
+		if (oldLat != lat) or (oldLong != long):
+			c.execute('UPDATE calls SET dblat=? where dbid=?', (lat, thisCall[0]))
+			c.execute('UPDATE calls SET dblong=? where dbid=?', (long, thisCall[0]))
+			db.commit()
+			callChanged = 1
+			whatChanged = whatChanged + "LatLong "
+		if oldNumUnits != numunits:
+			c.execute('UPDATE calls SET dbunitids=? where dbid=?', (unitids, thisCall[0]))
+			c.execute('UPDATE calls SET dbnumunits=? where dbid=?', (numunits, thisCall[0]))
+			db.commit()
+			callChanged=1
+			whatChanged = whatChanged + "Units "
+		if callChanged==1:  #If the call was changed, update the DB
+			c.execute('UPDATE calls SET dbupdatetime=? where dbid=?', (now, thisCall[0]))
+			db.commit()
+			c.execute('SELECT * FROM calls WHERE dbcalldate=?', (calldate,))
+			call = c.fetchone()
+			print "$" + str(call)
+			return 2, str(call[0]) + "\n" + whatChanged
+		else:  #call exists and wasn't updated
+			return 0, str(thisCall[0])
+	else:  #if this call is new
+		c.execute('INSERT INTO calls(dbcalldate, dbcalltype, dbstreet, dblat, dblong, dbunitids, dbnumunits, dbupdatetime) VALUES(?,?,?,?,?,?,?,?)', (calldate, calltype, address, lat, long, unitids, numunits, now))
+		db.commit()
+		c.execute('SELECT * FROM calls WHERE dbcalldate=?', (calldate,))
+		call = c.fetchone()
+		print "+" + str(call)
+		return 1, str(call[0])
+	return -1, "error"
+
+def sendTweet(fullCall):
+	tweet = twitter.Api(consumer_key=TWITTER_CONSUMERKEY,consumer_secret=TWITTER_CONSUMERSECRET,access_token_key=TWITTER_ACCESSKEY,access_token_secret=TWITTER_ACCESSSECRET)
+	status = tweet.PostUpdate(fullCall)
+	print "Tweet sent: " + fullCall
+	return
+
+def sendEmail(footer):
+	fromaddr = EMAIL_FROM
+	toaddr = EMAIL_TO
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject']= "SDFD: " + calltype + " @ " + address + " with " + str(numunits) + " units"
+	body = calltype + "\n" + address + "\n" + unitids + " (" + str(numunits) + ")\n@" + calldate + "\nhttp://maps.google.com/?q=" + str(lat) + "," + str(long) + "\nCall ID: " + footer
+	msg.attach(MIMEText(body, 'plain'))
+	email = smtplib.SMTP(EMAIL_SERVER, 587)
+	email.ehlo()
+	email.starttls()
+	email.ehlo()
+	email.login(EMAIL_FROM,EMAIL_PASS)
+	text = msg.as_string()
+	email.sendmail(EMAIL_FROM, EMAIL_TO, text)
+	email.quit()
+	print "Emailed: " + calltype + " @ " + calldate
+	return
+
+###Main:
+r = requests.get('https://webapp.pulsepoint.org/active_incidents.php?agencyid=37140&tz=420')
 soup = BeautifulSoup(r.text, 'html.parser')
-table = soup.find(lambda tag: tag.name=='table' and tag.has_attr("id") and tag['id']=="gv1")
+footer = "X"
+callDesc = " " 
 
-# Variables
-savedCallDate = "none"
-callDesc = []
+for row in soup.findAll("row"):  # Run through each row and pick apart the call details
+	call = row.findAll("cell")
+	if len(call) == 6:  #If the row contains probably good call data
+		calldate = str(call[0].get_text())
+		calltype = str(call[5].get_text())
+		address = str(call[2].get_text())
+		address = address.split(", SAN DIEGO", 1)[0]
+		unitids = BeautifulSoup(call[3].get_text(), 'html.parser')
+		unitids = str(unitids.get_text())
+		unitids = unitids.replace("?", "")
+		unitids = unitids.replace("^", "")
+		latlong = str(call[4].get_text())
+		lat = float(latlong.split(',')[0])
+		long = float(latlong.split(',')[1])
+		numunits = unitids.count(',') + 1
+		callDesc = "[" + calltype + "] " + address + ": " + unitids + " (" + str(numunits) + ") @ " + calldate + " http://maps.google.com/?q=" + str(lat) + "," + str(long)
+		savedType,savedFooter = storeCall() #Store the call. Reutrn type is 0 for old, 1 for new, 2 for update call. Footer is a string of dbid & what (if any) changed
+		if savedType == 1:  #If it is a new call
+			sendTweet(callDesc)
+		if savedType > 0:  #If the call is new or updated
+			if any(word in callDesc for word in COOLCALL):  #Check if it should email
+				sendEmail(savedFooter)
+			elif numunits > 7:  #Check if it should email
+				sendEmail(savedFooter)
+		elif savedType < 0:
+		  sendEmail("\nstoreCall() ERROR!!")
+			print "storeCall() ERROR!!"
+			exit()
 
-# Run through each row and pick apart the call details
-for row in table.findAll("tr"):
-  call = row.findAll("td")
-  if len(call) == 5: 
-    date = str(call[0].get_text().strip())
-    calltype = str(call[1].get_text().strip())
-    street = str(call[2].get_text().strip())
-    cross = str(call[3].get_text().strip())
-    unitid = str(call[4].get_text().strip())
-# Build the incident string, adding units each time. If a new incident is found, save the call.
-    if cross == '\x00':
-      cross2 = " "
-    else:
-      cross2 = " & " + cross + " "
-    if savedCallDate != date:
-      if savedCallDate != "none":
-        storeCall(callDesc)
-      callDesc = "[" + calltype + "] " + street + cross2 + "- " + unitid
-      units = unitid
-      numUnits = 1
-    else:
-      callDesc = callDesc + ", " + unitid
-      units = units + ", " + unitid
-      numUnits = numUnits + 1
-    savedCallDate = date
-    savedCallType = calltype
-    savedCallStreet = street
-    savedCallCross = cross
-# Save the last call built
-storeCall(callDesc)
-
-db.close()
 print "\nFinished."
 exit()
